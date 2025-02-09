2031b1eced7b89e430f8be61f841e3f449c6f2fd
mihahokk <mihahokk@gmail.com>
2025-02-09 23:08:40 +0300

    End of section 10

--- client/src/app/members/member-list/member-list.component.ts
+++ client/src/app/members/member-list/member-list.component.ts
@@ -13,16 +13,13 @@ import {MemberCardComponent} from "../member-card/member-card.component";
   styleUrl: './member-list.component.css'
 })
 export class MemberListComponent implements OnInit {
-  private memberService = inject(MembersService);
-  members: Member[] = [];
+  memberService = inject(MembersService);
 
   ngOnInit() {
-    this.loadMember()
+    if (this.memberService.members().length === 0) this.loadMember();
   }
 
   loadMember() {
-    this.memberService.getMembers().subscribe({
-      next: members => this.members = members
-    })
+    this.memberService.getMembers();
   }
 }
